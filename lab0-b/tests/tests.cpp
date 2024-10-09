1debb7e89e6a64bcf6cbb212cbdcd7dd2483ab19
LordFildzhion <dboechnick1@mail.ru>
2024-10-09 11:48:26 +0700

    Update files

--- lab0-b/tests/tests.cpp
+++
@@ -1,9 +0,0 @@
-#include "../lib/word_counter/word_counter.hpp"
-
-#include <gtest/gtest.h>
-#include "../lib/word_counter/word_counter.hpp"
-
-TEST(TestOpenFile, FileReadFail) {
-    word_counter wc("in.txt", "out.csv");
-    ASSERT_TRUE(1 == true);
-}
