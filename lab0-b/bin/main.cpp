c9e97e377ee1060e7b87d4afbfcb92e66ccea8c2
LordFildzhion <dboechnick1@mail.ru>
2024-10-12 01:40:19 +0700

    Update files

--- lab0-b/bin/main.cpp
+++ lab0-b/bin/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        std::cout << (argc < 3 ? "More arguettes are needed"
+        std::cout << (argc < 3 ? "More argumettes are needed"
                                : "Fewer arguments are needed");
         return 0;
     }
